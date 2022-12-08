7aec1c2f60d34b4bcdb134e49fa82084bd56b2f9
LiliiaBgdnv <109213422+LiliiaBgdnv@users.noreply.github.com>
2022-12-08 21:37:57 +0300

    Update using several flags and count of bytes

--- hw_sys_os/wc.py
+++ hw_sys_os/wc.py
@@ -1,29 +1,24 @@
-  GNU nano 4.8                                                                    wc.py                                                                              #!/usr/bin/env python3
+#!/usr/bin/env python3
 import sys
 import os
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=argparse.FileType(), default=sys.stdin, nargs='?')
-parser.add_argument('-l', '--lines', action='store_true', default=False, help='print the newline counts')
-parser.add_argument('-w', '--words', action='store_true', default=False, help='print the word counts')
-parser.add_argument('-c', '--count', action='store_true', default=False, help='print the byte counts')
+parser.add_argument('-l', '--lines', action='store_true', help='print the newline counts')
+parser.add_argument('-w', '--words', action='store_true', help='print the word counts')
+parser.add_argument('-c', '--count', action='store_true', help='print the byte counts')
 args = parser.parse_args()
 chars = words = lines = 0
 output=[]
-if args.count:
-    for line in args.file:
-        chars += len(line)
-    output.append(str(chars))
-    output.append(args.file.name)
-if args.words:
-    for line in args.file:
-        words += len(line.split())
-    output.append(str(words))
-    output.append(args.file.name)
+for line in args.file:
+    words += len(line.split())
+    lines += 1
 if args.lines:
-    for line in args.file:
-        lines += 1
     output.append(str(lines))
-    output.append(args.file.name)
+if args.words:
+    output.append(str(words))
+if args.count:
+    output.append(str(os.path.getsize(os.path.abspath(args.file.name))))
+output.append(args.file.name)
 output.append('\n')
 sys.stdout.write(' '.join(output))

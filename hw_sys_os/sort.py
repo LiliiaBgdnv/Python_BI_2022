d87d62010141291069db998aae049f3ffac5dadf
LiliiaBgdnv <109213422+LiliiaBgdnv@users.noreply.github.com>
2022-12-09 16:49:46 +0300

    Added ability to work in a conveyor belt

--- hw_sys_os/sort.py
+++ hw_sys_os/sort.py
@@ -2,7 +2,7 @@
 import sys
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('file', type=argparse.FileType(), default=sys.stdin)
+parser.add_argument('file', nargs='?', type=argparse.FileType(), default=sys.stdin)
 args = parser.parse_args()
 for line in sorted(args.file):
     sys.stdout.write(line)

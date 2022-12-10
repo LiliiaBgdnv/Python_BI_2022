d8e6624dd75066c3ef73462ef78da25bbb9cca13
LiliiaBgdnv <109213422+LiliiaBgdnv@users.noreply.github.com>
2022-12-10 18:04:47 +0300

    Update sort.py

--- hw_sys_os/sort.py
+++ hw_sys_os/sort.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import sys
 import argparse
-description = ''' sort [OPTION]... [FILE]...
-  or:  sort [OPTION]... --files0-from=F
-Write sorted concatenation of all FILE(s) to standard output.
+description = ''' ./sort.py [OPTION]... [FILE]...
+  or:  ./sort.py [OPTION]... --files0-from=F
+Write sorted concatenation of all FILE(s) to standard output.\n
 
-With no FILE, or when FILE is -, read standard input.
+With no FILE, or when FILE is -, read standard input.\n
 
-Mandatory arguments to long options are mandatory for short options too.'''
+Mandatory arguments to long options are mandatory for short options too.\n'''
 parser = argparse.ArgumentParser(description)
 parser.add_argument('file', nargs='?', type=argparse.FileType(), default=sys.stdin)
 args = parser.parse_args()

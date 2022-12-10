865eb4d45564024522b9af43aa52393559917d0d
LiliiaBgdnv <109213422+LiliiaBgdnv@users.noreply.github.com>
2022-12-10 17:54:10 +0300

    Add description

--- hw_sys_os/sort.py
+++ hw_sys_os/sort.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 import sys
 import argparse
-parser = argparse.ArgumentParser()
+description = ''' sort [OPTION]... [FILE]...
+  or:  sort [OPTION]... --files0-from=F
+Write sorted concatenation of all FILE(s) to standard output.
+
+With no FILE, or when FILE is -, read standard input.
+
+Mandatory arguments to long options are mandatory for short options too.'''
+parser = argparse.ArgumentParser(description)
 parser.add_argument('file', nargs='?', type=argparse.FileType(), default=sys.stdin)
 args = parser.parse_args()
 for line in sorted(args.file):
